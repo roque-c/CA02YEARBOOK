1495c613b08ce10b84fdad39daf4d66b8bbfff3f
roque-c <c.roque.prog@gmail.com>
2026-06-04 22:13:33 -0600

    fixed merge conflicts

--- yearbook/urls.py
+++ yearbook/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('class_of_2026/', views.StudentListView.as_view(), name='students'),
+    path('class_of_2026/<int:pk>', views.StudentDetailView.as_view(), name='students'),
     path('events/', views.EventListView.as_view(), name='events'),
 ]
